19d2b2944a509ccb42b2bdc7edee2c97db62c7fe
Darren Aitken <darrenaitken@users.noreply.github.com>
2021-03-27 11:44:58 +0000

    Set ref for search field and focus on load

--- src/components/searchField.jsx
+++ src/components/searchField.jsx
@@ -1,23 +1,32 @@
 // Node Modules
-import React from "react";
+import React, {useEffect, useRef} from "react";
 
 // CSS
 import "../styles/SearchField.css";
 
 const SearchField = () => {
 
+    const refSearchField = useRef()
+
+    // Load: Simulates componentDidMount() method.
+    // This will only run once - when the component has mounted
+    useEffect(() => {
+        refSearchField.current.focus();
+    },[])
+
 
     return (
         <div className="container-searchfield">
                 <input id="searchField" 
-                        class="search-field" 
+                ref={refSearchField}
+                        className="search-field" 
                         type="text" 
                         placeholder="Enter Postcode..." 
                         minLength="3" 
-                        maxlength="8" 
+                        maxLength="8" 
                         tabIndex="1" 
                         required></input>
-                <button id="btnSearch" type="submit"><i class="fa fa-search"></i></button>
+                <button id="btnSearch" type="submit"><i className="fa fa-search"></i></button>
 
         </div>
     )
